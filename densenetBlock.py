6983d60a8d5b58e92022e7bb0335c8460790de06
Nick Steelman <gns126@gmail.com>
2018-06-12 15:39:49 -0400

    decreasing loss

--- densenetBlock.py
+++ densenetBlock.py
@@ -12,15 +12,15 @@ import tensorflow as tf
 import tensorlayer as tl
 import numpy as np
 from tensorlayer.layers import *
-import numpy as np
-import importImg
-import saveMovie
 
 class block:
-    def __init__(self, name, labels):
+    def __init__(self, name, labels, input_shape):
         #Initialized Immediately
         self.name = name
         self.labels = labels #TRUE PREDICTIONS STARTING AT 0
+        self.input_shape = input_shape
+        self.input = tf.placeholder(tf.float32, shape=(None, input_shape[0], input_shape[1], input_shape[2]))
+
         #Should be Initialized before model initialization
         self.block_labels = None
         self.learning_rate = None
@@ -30,7 +30,8 @@ class block:
         self.input_shape = None
         #Initialized at model initialization
         self.output_shape = None
-        self.next_input = None
+        self.output = None
+        self.output_shape = None
         self.train_step = None
         #initializated when creating new models
         self.children = [] #TRUE PREDICTIONS STARTING AT 1
@@ -42,47 +43,48 @@ def define_block_body(x_image, block_info, reuse = False):
     with tf.variable_scope(prefix + "block") as scope:
         if reuse: #get previous variable if we are reusing the discriminator but with fake images
             scope.reuse_variables()
-        # inputs = InputLayer(x_image, name=prefix + 'block_inputs')
         inputs = x_image
         conv_pointers = [inputs]#list of filters
         for i,v in enumerate(m.convolutions):
-            if i < len(convolutions)-1:
-                curr_layer = BatchNormLayer(Conv2d(conv_pointers[-1],
-                    abs(convolutions[i+1]), (5, 5),strides = (1,1), name=prefix +
-                    'conv1_%s'%(i)), act=tf.nn.leaky_relu,is_train=True ,name=prefix +
-                    'batch_norm%s'%(i))
-
+            curr_layer = BatchNormLayer(Conv2d(conv_pointers[-1],
+                m.convolutions[i], (5, 5),strides = (1,1), name=prefix +
+                'conv1_%s'%(i)), act=tf.nn.leaky_relu,is_train=True ,name=prefix +
+                'batch_norm%s'%(i))
+            if i < len(m.convolutions)-1:
                 conv_pointers.append(ConcatLayer([curr_layer, conv_pointers[-1]],
                  3, name =prefix + 'concat_layer%s'%(i)))
             else:
-                # fully connected layer
-                # l,w,d = m.input_size[0], m.input_size[1], sum(m.convolutions)
-                flat = FlattenLayer(conv_pointers[-1], name = prefix + 'flatten')
-                y_conv = DenseLayer(flat, m.fully_connected_size, name = prefix + 'hidden_encode')
+                conv_pointers.append(curr_layer)
+
+        flat = FlattenLayer(conv_pointers[-1], name = prefix + 'flatten')
+        y_conv = DenseLayer(flat, m.fully_connected_size, act=tf.nn.relu,name = prefix + 'hidden_encode')
+
         return y_conv, conv_pointers[-1]
 
-def define_block(x_image, block_info, reuse_body = False, reuse = False):
+def define_block(block_info, reuse_body = False, reuse = False):
     '''Handle the final fully connected layer of the block as well as the necessary
     variables to return'''
     m = block_info
-    hidden, lastLayer = define_block_body(x_image, block_info, reuse_body)
     prefix = m.name + '_'
+    tl_input = InputLayer(m.input, name= prefix +'tl_inputs')
+    hidden, lastLayer = define_block_body(tl_input, m, reuse_body)
     with tf.variable_scope(prefix + "block") as scope:
         if reuse: #get previous variable if we are reusing the discriminator but with fake images
             scope.reuse_variables()
-        m.y_conv = DenseLayer(flat, m.fully_connected_size, name = prefix + 'output')
+        m.y_conv = DenseLayer(hidden, len(m.labels), name = prefix + 'output').outputs
         m.y = tf.placeholder(tf.float32, shape=[None, len(m.labels)], name= prefix +'class_inputs') #correct class
 
-        cross_entropy = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels = m.y, logits = m.y_conv))# reduce mean for discriminator
+        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = m.y, logits = m.y_conv))# reduce mean for discriminator
         m.cross_entropy_summary = tf.summary.scalar(prefix + 'loss', cross_entropy)
         accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(m.y_conv,1), tf.argmax(m.y,1)), tf.float32))
         m.accuracy_summary = tf.summary.scalar(prefix + 'accuracy', accuracy)
 
-        m.next_input = MaxPool2d(lastLayer, filter_size = (2,2))
-        _, l, w, d = tf.shape(m.next_input.output)
+        m.output = MaxPool2d(lastLayer, filter_size = (2,2)).outputs
+        print(m.output.get_shape())
+        _, l, w, d = m.output.get_shape()
         m.output_shape = (l, w, d)
         t_vars = tf.trainable_variables()
         b_vars = [var for var in t_vars if prefix in var.name] #find trainable discriminator variable
         for var in b_vars:
             print(var.name)
-        m.train_step = tf.train.AdamOptimizer(m.learning_rate, beta1=m.beta1).minimize(d_cross_entropy, var_list=b_vars)
+        m.train_step = tf.train.AdamOptimizer(m.learning_rate, beta1=m.beta1).minimize(cross_entropy, var_list=b_vars)
--- trainer.py
+++ trainer.py
@@ -7,30 +7,45 @@
 # from tensorflow.examples.tutorials.mnist import input_data
 # mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 import os
-os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+# os.environ["CUDA_VISIBLE_DEVICES"]="-1"
 import tensorflow as tf
 import tensorlayer as tl
 import numpy as np
 from tensorlayer.layers import *
-import numpy as np
-import matplotlib.pyplot as plt
-import importImg
-import saveMovie
-import denseBlock
+import densenetBlock
 import setParameters
-import pickle
+import cPickle
 
-NUM_CLASSES = 200
+NUM_CLASSES = 101
+ITERATIONS = 100000
+BATCH_SIZE = 50
+WHEN_SAVE = 10
+
+DATA_PATH = '/home/gtower/Data/cifar-100-python/'
+TRAINING_NAME = 'train'
+TESTING_NAME = 'test'
+INPUT_SHAPE = 32, 32, 3
+NUM_OUTPUTS = 5
+
+LEARNING_RATE = 1e-3
+BETA1 = .9
+CONVOLUTIONS = [32, 64]
+FULLY_CONNECTED_SIZE = 1024
+
+RESTORE = False
+
+MODEL_FILEPATH = '/home/gtower/Models/CIFAR100/model.ckpt' #filepaths to model and summaries
+SUMMARY_FILEPATH = '/home/gtower/Models/CIFAR100/Summaries/'
 
 def new_block(parent, index, list_classes):
     '''The block is the container for an individual network. This function creates
     a new block that predicts from a list of given classes'''
-    m = block(parent.name + index, list_classes)
-    m.block_labels = np.zeros(NUM_CLASSES)#Labels specific to the indexing of this block
+    m = densenetBlock.block(parent.name + index, list_classes)
+    m.block_labels = np.zeros(NUM_CLASSES, dtype=np.int16)#Labels specific to the indexing of this block
     m.block_labels[block_info.labels] = np.arange(len(block_info.labels)) + 1
     if len(m.labels) > 1:#If there is more than one class.
         setParameters.set_parameters(m, parent)
-        define_block(parent.next_input, m)
+        define_block(m)
     update_dict(m)
     return m
 
@@ -46,7 +61,6 @@ def generate_children(block_info):
         group = block_info.labels[group]
         block = new_block(block_info, str(index + 1), group)
         block_info.chlidren.append(block)
-
 
 def get_confusion_matrix(block_info, batch_size, num_batches):
     '''Generates a confusion matrix from the data'''
@@ -68,37 +82,105 @@ def get_confusion_matrix(block_info, batch_size, num_batches):
 def update_dict(block_info):
     '''Update the global dictionary mapping labels to their path.'''
     for i, label in enumerate(block_info.labels):
-        path.update(label, (m.name, str(i+1)))
+        path[label] = (block_info.name, str(i+1))
 
-def train_block(block_info, input_x, labels, input_placeholder):
+def train_block(block_info, input_x, labels):
     '''Train an individual block. First we must reconfigure the labels to put them
     in the appropriate indeces for the model, then we train it. Then repeat the
     process with its children.'''
+    m = block_info
     block_labels_curr = block_info.block_labels[labels]
-    #TODO
-    #Create Global Variables sess, train_writer, time_step
-    train_dict = {input_placeholder: input_x, block_info.y: block_labels_curr}
-    _, y_conv, cross_entropy_summary, accuracy_summary = sess.run([m.train_step, m.y_conv, m.cross_entropy_summary, m.accuracy_summary],
+    indexes = np.arange(len(block_labels_curr))
+    one_hot_labels = np.zeros((len(block_labels_curr),len(block_info.labels)))
+    one_hot_labels[indexes, block_labels_curr] = 1
+    feed_dict = {m.input: input_x, m.y: one_hot_labels}
+    _, output, y_conv, cross_entropy_summary, accuracy_summary = sess.run([m.train_step,
+     m.output, m.y_conv, m.cross_entropy_summary, m.accuracy_summary],
     feed_dict=feed_dict)#train generator)
+    print(one_hot_labels[0:20])
+    print(block_labels_curr[0:20])
+    print(labels[0:20])
     train_writer.add_summary(cross_entropy_summary, time_step)
     train_writer.add_summary(accuracy_summary, time_step)
 
     for child in block_info.children:
         if len(child.labels) > 1:
-            next_input_x = np.where(np.isin(labels, child.labels) | np.isin(y_conv, child.labels),input_x)
+            next_input_x = np.where(np.isin(labels, child.labels) | np.isin(y_conv, child.labels),output)
             next_labels = np.where(np.isin(labels, child.labels) | np.isin(y_conv, child.labels),labels)
-            train_block(child, next_input_x, next_labels, input_placeholder)
-
-
+            train_block(child, next_input_x, next_labels)
 def restore_models():
     pass
 
+def define_head(list_classes):
+    head_block = densenetBlock.block('S', list_classes, INPUT_SHAPE)
+    head_block.block_labels = np.zeros(NUM_CLASSES, dtype=np.int16)#Labels specific to the indexing of this block
+    head_block.block_labels[head_block.labels] = np.arange(len(head_block.labels)) + 1
+    head_block.learning_rate = LEARNING_RATE
+    head_block.beta1 = BETA1
+    head_block.convolutions = CONVOLUTIONS
+    head_block.fully_connected_size = FULLY_CONNECTED_SIZE
+    densenetBlock.define_block(head_block)
+    update_dict(head_block)
+    return head_block
+
+def train_model(head_block ,num_iterations):
+    global time_step
+    time_step = 0
+    batch_size = BATCH_SIZE
+    my_gen = get_batch_generator(batch_size, DATA_PATH+TRAINING_NAME)
+    epoch = 0
+    input_images_summary = tf.summary.image("image_inputs", head_block.input ,max_outputs = NUM_OUTPUTS)
+
+    for iteration in range(num_iterations):
+        time_step += 1
+        if time_step % 10 ==0:
+            print(time_step)
+        x_batch, y_labels = next(my_gen,(None, None))
+
+        while x_batch is None:#when the generator is done, instantiate a new one
+            my_gen = get_batch_generator(batch_size, DATA_PATH+TRAINING_NAME)
+            epoch += 1
+            print("Completed Epoch. Num Completed: ",epoch)
+            x_batch, y_labels = next(my_gen,(None, None))
+
+        input_x = np.reshape(x_batch, [len(y_labels), INPUT_SHAPE[2], INPUT_SHAPE[0], INPUT_SHAPE[1]])
+        input_x = np.transpose(input_x, (0, 2, 3, 1))
+        train_block(head_block, input_x, y_labels)
+        if iteration % WHEN_SAVE ==0:
+            saver.save(sess, MODEL_FILEPATH)
+        if iteration % 1 ==0:
+            curr_images_summary = sess.run(input_images_summary, feed_dict={head_block.input: input_x})
+            train_writer.add_summary(curr_images_summary, time_step)
+
+
+def get_batch_generator(batch_size, file_name):
+    data_dict = unpickle(file_name)
+    data = data_dict['data']
+    labels = data_dict['fine_labels']
+    for i in range(0,len(labels),batch_size):
+        j = min(i+batch_size,len(labels))
+        yield data[i:j], labels[i:j]
+
+def unpickle(file_name):
+    with open(file_name, 'rb') as fo:
+        data_dict = cPickle.load(fo)
+    return data_dict
 
 if __name__ == "__main__":
     #paths is a dictionary containing the binary search paths to each
     #PATHS START AT 1
-    paths = {} #THERE SHOULD NOT BE A 0 IN ANY STRINGS
+    path = {} #THERE SHOULD NOT BE A 0 IN ANY STRINGS
+    sess = tf.Session()#start the session
     classes = np.arange(NUM_CLASSES)
-    m = new_block("S", None, classes)
-    for i in range(NUM_CLASSES):
-        paths.update(classes[i],("S" ,str(i+1)))
+    head_block = define_head(classes)
+    saver = tf.train.Saver()#saving and tensorboard
+    sess.run(tf.global_variables_initializer())
+    train_writer = tf.summary.FileWriter(SUMMARY_FILEPATH,
+                                  sess.graph)
+    if MODEL_FILEPATH is not None and RESTORE:
+        saver.restore(sess, MODEL_FILEPATH)
+    else:
+        print('SAVE')
+        saver.save(sess, MODEL_FILEPATH)
+
+    train_model(head_block , ITERATIONS)
